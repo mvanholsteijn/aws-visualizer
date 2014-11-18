10ae025c51f1fa0da6a2c8179133d1c6914ad4ff
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2014-11-18 22:14:02 +0100

    objectified :-)

--- graph_region.py
+++ graph_region.py
@@ -1,43 +1,52 @@
 import boto.ec2
 from boto.vpc import VPCConnection
 
-EC2 = boto.ec2.connect_to_region('eu-west-1')
-VPC = boto.vpc.connect_to_region('eu-west-1')
 
+class AWSVisualizer:
 
-def get_all_security_groups_in_vpc(vpc):
-     result = {}
-     all_groups = EC2.get_all_security_groups(filters={'vpc-id': vpc})
-     for group in all_groups:
-	result[group.id] = group
-     return result
+	def __init__(self):
+		self.vpcs = None
+		self.instances = None
+		self.security_groups = None
+		self.region = 'eu-west-1'
+		self.EC2 = None
+		self.VPC = None
 
+	def connect(self):
+		self.EC2 = boto.ec2.connect_to_region('eu-west-1')
+		self.VPC = boto.vpc.connect_to_region('eu-west-1')
 
-def print_sgs(sgs):
-    for id in sgs:
-	group = sgs[id]
-	for rule in group.rules:
-		print rule.from_port
-		print rule.to_port
-		if len(rule.grants):
-			for grant in rule.grants:
-				print grant.__dict__ 
-	
-print "digraph region {"
-vpcs = VPC.get_all_vpcs()
-for vpc in vpcs:
-     groups = get_all_security_groups_in_vpc(vpc.id)
-     subnets = VPC.get_all_subnets(filters={'vpc-id': vpc.id})
-     for subnet in subnets:
-	instances = EC2.get_only_instances(filters={'subnet-id': subnet.id}, max_results=200)
-	if len(instances):
-		name = subnet.tags['Name'] if 'Name' in subnet.tags else subnet.id
-		print 'subgraph "cluster-%s" {' % (subnet.id)
-		print 'label = "%s;"' % name;
-		for instance in  instances:
-			name = instance.tags['Name'] if 'Name' in instance.tags else instance.id
-			print '"%s" [label="%s"];' % (instance.id, name)
-			for group in instance.groups:
-				print '//   %s has security group %s ' % (instance.id, group.id)
+	def load(self):
+		self.security_groups = self.EC2.get_all_security_groups()
+		self.instances = self.EC2.get_only_instances(max_results=200)
+		self.vpcs = self.VPC.get_all_vpcs()
+		self.subnets = {}
+		for vpc in self.vpcs:
+			self.subnets[vpc.id] = self.VPC.get_all_subnets(filters={'vpc-id': vpc.id})
+
+	def get_instances_in_subnet(self, subnet_id):
+		result = []
+		for instance in self.instances:
+			if instance.subnet_id == subnet_id:
+				result.append(instance)
+		return result
+
+	def print_dot(self):
+		print "digraph region {"
+		for vpc in self.vpcs:
+			for subnet in self.subnets[vpc.id]:
+				subnet_instances = self.get_instances_in_subnet(subnet.id)
+				if len(subnet_instances):
+					name = subnet.tags['Name'] if 'Name' in subnet.tags else subnet.id
+					print 'subgraph "cluster-%s" {' % (subnet.id)
+					print 'label = "%s;"' % name;
+					for instance in  subnet_instances:
+						name = instance.tags['Name'] if 'Name' in instance.tags else instance.id
+						print '"%s" [label="%s"];' % (instance.id, name)
+					print '}'
 		print '}'
-print '}'
+
+visualizer = AWSVisualizer()
+visualizer.connect()
+visualizer.load()
+visualizer.print_dot()
