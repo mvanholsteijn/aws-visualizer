717ea2e2415978d726ecc3260bf4cc35c828ac0d
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2014-11-19 17:37:59 +0100

    added all views in one run

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
-mkdir -p target
-rm -rf target/*.dot target/*.png
-python graph_region.py  --directory target $@
-for file in target/*.dot; do
-	dot -Tpng -o target/$(basename $file .dot).png  $file
+rm -rf target
+mkdir -p target/default
+mkdir -p target/external
+mkdir -p target/subnets
+
+python graph_region.py  --directory target/default 
+python graph_region.py  --directory target/subnets --use-subgraphs 
+python graph_region.py  --directory target/external --show-external-only --use-subgraphs 
+
+
+for file in target/*/*.dot; do
+	dot -Tpng -o $(dirname $file)/$(basename $file .dot).png  $file
 done
-open target/*.png
+open target/*/*.png
